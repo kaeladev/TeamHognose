afda6c432030154125177eb2bd6b7ab985a61852
kaeladev <kaeladev@gmail.com>
2025-07-02 01:33:18 +0200

    Initial Setup

---
+++ Assets/Scripts/MB_Test.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class MB_Test : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        bool Test = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
