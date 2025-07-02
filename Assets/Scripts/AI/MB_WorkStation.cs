6a7c93eb25c1fa52e3c25534ec11d4d69c1feded
kaeladev <kaeladev@gmail.com>
2025-07-02 23:58:59 +0200

    First Day First Passes
    - Some placeholder Tortilla anims + first test setup of anim BP
    - Initial pass of working NPC and workstation classes
    - Some basic folders and scenes
    - Removing test script

---
+++ Assets/Scripts/AI/MB_WorkStation.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+abstract public class MB_WorkStation : MonoBehaviour
+{
+    public MB_NPCBehavior_Work NPC;
+
+    // 0.0-1.0 value aka 0% to 100%
+    protected float WorkCompletionPercentage = 0.0f;
+
+    // What % of work is completed per second when in use?
+    public float RateOfCompletion = 1.0f;
+     
+    void Start()
+    {
+        if (!NPC)
+        {
+            Debug.Log("Did not find NPC in WorkStation: " + this.ToString());
+        }
+    }
+
+    void Update()
+    {
+        if (NPC.IsWorking && WorkCompletionPercentage < 1)
+        {
+            WorkCompletionPercentage += ((RateOfCompletion / 100.0f) * Time.deltaTime);
+            WorkCompletionPercentage = Mathf.Min(WorkCompletionPercentage, 1.0f);
+        }
+
+        DisplayStationCompletion();
+    }
+
+    // Each station will display their completion differently
+    abstract public void DisplayStationCompletion();
+
+    // 
+    public bool HasAmount(float Amount) { return WorkCompletionPercentage >= Amount; }
+}
